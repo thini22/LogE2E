a8f06eb0134659aae0e385d3230f6acc7491281d
thini22 <thinidemo@gmail.com>
2024-06-15 11:37:46 +0530

    Update e2e.spec.js

--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -63,6 +63,5 @@ test('@Web Client App login', async ({ page }) => {
    const orderIdDetails = await page.locator(".col-text").textContent();
    expect(orderId.includes(orderIdDetails)).toBeTruthy();
   console.log("Test completed");
-   console.log("Test completed");
-   console.log("Test completed");
+   
 });
